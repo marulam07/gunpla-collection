bbd1ba8f628cc799a4efbe20e83a054d5ab01360
M Haidar Hanif <git@mhaidarhanif.com>
2017-10-13 10:57:36 +0700

    Connect frontend with backend via API

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,15 @@ const gunplaCollection = [
   }
 ]
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+
 app.get("/", (req, res) => {
   res.send(gunplaCollection)
 })
--- frontend/index.js
+++ frontend/index.js
@@ -1,43 +1,44 @@
 ;(function() {
-  // console.log("HELLO GUNPLA")
+  $.get("http://localhost:3000", data => {
+    console.log(data)
 
-  let gunplaCollection = [
-    {
-      id: 0,
-      model: "DUMMY",
-      year: 2000,
-      price: 100000
-    },
-    {
-      id: 1,
-      model: "Gundam Barbatos",
-      year: 2015,
-      price: 800000
-    },
-    {
-      id: 2,
-      model: "Gundam Unicorn",
-      year: 2010,
-      price: 1200000
-    },
-    {
-      id: 3,
-      model: "Gundam RX",
-      year: 2000,
-      price: 3200000
-    }
-  ]
+    let gunplaCollection = [
+      {
+        id: 0,
+        model: "DUMMY",
+        year: 2000,
+        price: 100000
+      },
+      {
+        id: 1,
+        model: "Gundam Barbatos",
+        year: 2015,
+        price: 800000
+      },
+      {
+        id: 2,
+        model: "Gundam Unicorn",
+        year: 2010,
+        price: 1200000
+      },
+      {
+        id: 3,
+        model: "Gundam RX",
+        year: 2000,
+        price: 3200000
+      }
+    ]
 
-  // console.log(gunplaCollection)
+    // console.log(gunplaCollection)
 
-  gunplaCollection.map(item => {
-    const id = item.id
-    const model = item.model
-    const year = item.year
-    const price = item.price
-    // console.log(id, model, year, price)
+    gunplaCollection.map(item => {
+      const id = item.id
+      const model = item.model
+      const year = item.year
+      const price = item.price
+      // console.log(id, model, year, price)
 
-    const newRow = `
+      const newRow = `
       <tr>
         <th scope="row">${id}</th>
         <td>${model}</td>
@@ -45,8 +46,9 @@
         <td>${price}</td>
       </tr>
       `
-    // console.log(newRow)
+      // console.log(newRow)
 
-    $("#app-table-body").append(newRow)
+      $("#app-table-body").append(newRow)
+    })
   })
 })()
