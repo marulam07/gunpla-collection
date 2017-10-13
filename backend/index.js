6d79c9c418bf738689d345df62a7ddbee67ee60e
M Haidar Hanif <git@mhaidarhanif.com>
2017-10-13 10:50:45 +0700

    Build initial API

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+
+const gunplaCollection = [
+  {
+    id: 0,
+    model: "DUMMY",
+    year: 2000,
+    price: 100000
+  },
+  {
+    id: 1,
+    model: "Gundam Barbatos",
+    year: 2015,
+    price: 800000
+  },
+  {
+    id: 2,
+    model: "Gundam Unicorn",
+    year: 2010,
+    price: 1200000
+  },
+  {
+    id: 3,
+    model: "Gundam RX",
+    year: 2000,
+    price: 3200000
+  }
+]
+
+app.get("/", (req, res) => {
+  res.send(gunplaCollection)
+})
+
+app.listen(3000, () => {
+  console.log("API running on localhost:3000")
+})
