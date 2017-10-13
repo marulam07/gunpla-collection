da740e610e1bc9da884b9c74044901e7177c37c0
M Haidar Hanif <git@mhaidarhanif.com>
2017-10-13 10:31:22 +0700

    Append data into table

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,5 @@
 ;(function() {
-  console.log("HELLO GUNPLA")
+  // console.log("HELLO GUNPLA")
 
   let gunplaCollection = [
     {
@@ -28,5 +28,25 @@
     }
   ]
 
-  console.log(gunplaCollection)
+  // console.log(gunplaCollection)
+
+  gunplaCollection.map(item => {
+    const id = item.id
+    const model = item.model
+    const year = item.year
+    const price = item.price
+    // console.log(id, model, year, price)
+
+    const newRow = `
+      <tr>
+        <th scope="row">${id}</th>
+        <td>${model}</td>
+        <td>${year}</td>
+        <td>${price}</td>
+      </tr>
+      `
+    // console.log(newRow)
+
+    $("#app-table-body").append(newRow)
+  })
 })()
