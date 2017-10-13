44f62f2896567a27743dd3f72d60cef58d40d008
M Haidar Hanif <git@mhaidarhanif.com>
2017-10-13 10:24:08 +0700

    Build dummy data

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,32 @@
-console.log("HELLO GUNPLA")
+;(function() {
+  console.log("HELLO GUNPLA")
+
+  let gunplaCollection = [
+    {
+      id: 0,
+      model: "DUMMY",
+      year: 2000,
+      price: 100000
+    },
+    {
+      id: 1,
+      model: "Gundam Barbatos",
+      year: 2015,
+      price: 800000
+    },
+    {
+      id: 2,
+      model: "Gundam Unicorn",
+      year: 2010,
+      price: 1200000
+    },
+    {
+      id: 3,
+      model: "Gundam RX",
+      year: 2000,
+      price: 3200000
+    }
+  ]
+
+  console.log(gunplaCollection)
+})()
